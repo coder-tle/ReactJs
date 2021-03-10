bcfd3ca57bd8494456035c9c92a15e418e0abf6b
Sarfaraz Alam <sarfarazalam.sa53@gmail.com>
2021-03-10 23:58:13 +0530

    understood file structure, public folder contains index.html, src folder contains index.js, understood usage of each file & got basic understanding of file structure of react app

---
+++ react-task-tracker/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* inserts app into the div 'root' */}
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
