b7674d44198b3cc5d6a03d6bed3b2debffcda357
Sarfaraz Alam <sarfarazalam.sa53@gmail.com>
2021-03-12 00:29:38 +0530

    created tasks and task components

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,13 +1,36 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
-  
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false,
+    },
+
+  ])
   return (
     <div className='container'>
-      
+
       <Header />
+      <Tasks tasks={tasks} />
     </div>
-    
+
   );
 }
 
