e7c6712baa4c1064633e669f84236f2129f6e8e8
Sarfaraz Alam <sarfarazalam.sa53@gmail.com>
2021-03-26 23:44:36 +0530

    worked on form : added event listener to submit form & displayed details of task in the console by storing it as an object

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -25,6 +25,11 @@ function App() {
     },
   ]);
 
+  // Add Task
+  const addTask = (task) => {
+    console.log(task);
+  };
+
   // Delete Task
   const deleteTask = (id) => {
     console.log("delete", id);
@@ -52,7 +57,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
--- react-task-tracker/src/components/AddTask.js
+++ react-task-tracker/src/components/AddTask.js
@@ -1,22 +1,60 @@
-const AddTask = () => {
+import { useState } from "react";
+
+const AddTask = ({ onAdd }) => {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!text) {
+      alert("Please enter the task");
+      return;
+    }
+    onAdd({ text, day, reminder });
+    setText("");
+    setDay("");
+    setReminder(false);
+  };
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={onSubmit}>
       <div className="form-control">
         <label>Task</label>
-        <input type="text" placeholder="Add Task"></input>
+        <input
+          type="text"
+          placeholder="Add Task"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></input>
       </div>
 
       <div className="form-control">
         <label>Day & Time</label>
-        <input type="text" placeholder="Add Day & Time"></input>
+        <input
+          type="text"
+          placeholder="Add Day & Time"
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+        ></input>
       </div>
 
       <div className="form-control">
         <label>Set Reminder</label>
-        <input type="Checkbox"></input>
+        <input
+          type="Checkbox"
+          checked={reminder}
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        ></input>
       </div>
 
-      <input type="submit" value="Save Task" className="btn btn-block"></input>
+      <input
+        type="submit"
+        value="Save Task"
+        className="btn btn-block"
+        // onSubmit={onSubmit} this was creating problem
+      />
     </form>
   );
 };
